3c8b2f6a9a5b689d30e8087e238c73396b8c35f6
FariasMi <mi.farias182@gmail.com>
2021-07-14 19:14:50 -0300

    solving calculator problems

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,41 @@
-const add = function() {
+const add = function(a,b) {
+  return a+b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  return a-b;
+
 	
 };
 
-const sum = function() {
-	
+  const sum = function(arr){
+    const reducer = (sum, val) => sum + val;
+    const initialValue = 0;
+    return arr.reduce(reducer,initialValue);
+    
+
 };
 
-const multiply = function() {
 
+const multiply = function(arr) {
+  return arr.reduce((acc,item)=> acc *= item);
 };
 
-const power = function() {
-	
+const power = function(a,b){
+	let total = 1;
+	for(i=0; i < b; i++){
+	total *= a;
+		}
+    return total;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(n) {
+  if (n === 0 || n === 1 ){
+      return 1;
+    }
+  return n * factorial (n - 1);
+  }
 
 module.exports = {
   add,
